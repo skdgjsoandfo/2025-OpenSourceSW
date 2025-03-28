f17a0e6793fd5c8b535843fa7c7e8dffc1b98eeb
skdgjsoandfo <star020203@naver.com>
2025-03-28 10:39:09 +0900

    소스코드 업로드

---
+++ main.py
@@ -0,0 +1,80 @@
+#----------------------------------------------
+#num = input('정수 혹은 소수를 입력하세요:')
+
+#float_num = float(num)
+#print(f'임력한 숫자의 소수형: {float_num}')
+
+#int_num = int(float(num))
+#print(f'입력한 숫자의 정수형: {int_num}')
+#______________________________________________
+#string = input('영어로 아무거나 입력하세요 (5자 이상)')
+
+#print(f'str.upper(): {string.upper()}')
+#print(f'str.lower(): {string.lower()}')
+#print(f'str[2:4]: {string[2:4]}')
+#-----------------------------------------------
+#l = list()
+
+#l.append(4)
+#print(l)
+
+#l.append('String')
+#print(l)
+
+#l.append(10.44)
+#print(l)
+
+#l.remove('String')
+#print(l)
+#-------------------------------------------------
+#s = set()
+
+#s.add(10)
+#print(s)
+
+#s.add('String')
+#print(s)
+
+#s.add(14)
+#print(s)
+
+#s.remove('String')
+#print(s)
+#---------------------------------------------------
+#d = dict()
+
+#d['Key] = 'Value'
+#print(d)
+
+#d[4] = 'String'
+#print(d)
+
+#d[20.2] = 99.4
+#print(d)
+
+#d.pop(4)
+#print(d)
+#--------------------------------------------------
+#y = set()
+#for n in range(0,20):
+#    y.add(n)
+#    print(y)
+
+#---------------------------------------------------
+#fruits = {
+#    '사과': 1000,
+#    '바나나': 2000,
+#    '자두': 500,
+#    '복숭아': 4000,
+#}
+#fruits_name = input("어서옵쇼~ 어떤 과일을 찾으세요?")
+
+#if fruits_name in fruits:
+#    print(f'아! {fruits_name}는 {fruits[fruits_name]}원입죠~')
+#else:
+#    print(f"아이고~ 매장에 없네요.")
+#--------------------------------------------------------
+
+x = int(input())
+y = int(input))
+print(f'{x} +{y} = {x+y}')
